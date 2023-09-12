183bfc93aef46c35f1939760575672ed5cc59381
ermesonalvesgit <ermesonalvesart@gmail.com>
2023-09-11 22:34:46 -0300

    Add lib and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,97 @@
+import { FileVideo, Github, Upload } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+
 function App() {
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">Upload.ia</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-zinc-400">
+            Desenvolvido com ❤ na NLW.IA
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      {/* Main */}
+      <div className="flex-1 gap-6 flex p-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o prompt para I.A..."
+            />
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Resultado gerado pela I.A"
+              readOnly
+            />
+          </div>
+
+          <p className="text-zinc-500 text-sm">
+            Lembre:se você pode ultilizar a varaiavel{" "}
+            <code className="text-violet-700">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          {/* Foms */}
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm gap-2 flex-col items-center justify-center text-zinc-400 hover:bg-zinc-100/10"
+            >
+              <FileVideo className="h4 w-4" />
+              Selecione um vídeo
+            </label>
+            <input
+              type="file"
+              name="video"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label className="" htmlFor="transcription-prompt">
+                Prompt de Transcrição
+              </Label>
+              <Textarea
+                id="transcription-prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chave mencionadas no vídeo seprados por virgula [,]"
+              ></Textarea>
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="spacey-y-2">
+              <Label>Modelo</Label>
+            </div>
+          </form>
+        </aside>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
